1424a74ab3ab889213ea6f6cb277ecef0233a166
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-09-25 22:57:05 +0300

    создали клас авто и сделали обьекты

--- src/Car.java
+++ src/Car.java
@@ -6,6 +6,15 @@ public class Car {
     private int productionYear;
     private String productionCountry;
 
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+    }
+
     public String getBrand() {
         return brand;
     }
