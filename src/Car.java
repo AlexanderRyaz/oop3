1071cfad195b9989befd75fd1622f0b79a1a1f51
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-09-25 12:38:08 +0300

    создали клас авто и сделали обьекты

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,68 @@
 public class Car {
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int productionYear;
+    private String productionCountry;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public void setProductionCountry(String productionCountry) {
+        this.productionCountry = productionCountry;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", productionYear=" + productionYear +
+                ", productionCountry='" + productionCountry + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Car car1 = new Car();
+        car1.setBrand("Lada");
+        car1.setModel("Grande");
+        car1.setProductionYear(2015);
+        car1.setProductionCountry("Россия");
+        car1.setColor("Желтый цвет");
+        car1.setEngineVolume(1.7);
+        System.out.println(car1);
+        Car car2 = new Car();
+        car2.setBrand("Audi ");
+        car2.setModel("A8 50 L TDI quattro");
+        car2.setProductionYear(2020);
+        car2.setProductionCountry("Германия");
+        car2.setColor("черный");
+        car2.setEngineVolume(3.0);
+        System.out.println(car2);
+        Car car3 = new Car();
+        car3.setBrand("BMW");
+        car3.setModel("Z8");
+        car3.setProductionCountry("Германия");
+        car3.setProductionYear(2021);
+        car3.setColor("черный");
+        car3.setEngineVolume(3.0);
+        System.out.println(car3);
+        Car car4 = new Car();
+        car4.setBrand("Kia");
+        car4.setModel("Sportage 4 поколение");
+        car4.setProductionYear(2018);
+        car4.setProductionCountry("Южная Корея");
+        car4.setColor("красный");
+        car4.setEngineVolume(2.4);
+        System.out.println(car4);
+        Car car5 = new Car();
+        car5.setBrand("Hyundai");
+        car5.setModel("Avante");
+        car5.setProductionCountry("Южная Корея");
+        car5.setColor("оранжевый");
+        car5.setEngineVolume(1.6);
+        car5.setProductionYear(2016);
+        System.out.println(car5);
+
     }
 }
